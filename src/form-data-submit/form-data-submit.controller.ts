e8caa1d8d3c8a8cfa27b6b3f72ef9b919e181b24
Oliver Sim <choo-howe.sim@edag.com.my>
2023-06-09 13:40:56 +0800

    format: prettier

--- src/form-data-submit/form-data-submit.controller.ts
+++ src/form-data-submit/form-data-submit.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { FormDataSubmitService } from './form-data-submit.service';
 import { UpdateFormDataSubmitDto } from './dto/update-form-data-submit.dto';
 
